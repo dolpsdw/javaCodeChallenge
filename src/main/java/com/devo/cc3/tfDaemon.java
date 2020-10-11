77ced9e7f06042c86e03402f03ddf8e082a854ca
dolpsdw <dolpsdw@gmail.com>
2020-10-11 13:35:13 +0200

    Read the message for full changeLog
    ProcessLogFileResult algorithm changed.
    Implemented Flow from ProcessLogFileResult to calculate TF_IDF to all files

--- src/main/java/com/devo/cc3/tfDaemon.java
+++ src/main/java/com/devo/cc3/tfDaemon.java
@@ -1,6 +1,9 @@
 package com.devo.cc3;
 
-import io.reactivex.rxjava3.core.Flowable;
+import static java.lang.Math.log;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.BehaviorSubject;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -13,15 +16,85 @@ import org.apache.commons.io.LineIterator;
 public class tfDaemon {
 
     public static void main(String[] args) {
-        Flowable.just("Hello world").subscribe(System.out::println);
-        File testFile = new File("C:\\Users\\Jesus\\debug.log");
+        // Sanitize input terms by toLower and remove duplicates with HashSet
         HashSet<String> testInput = new HashSet<>();
-        testInput.add("Error");
-        testInput.add("Warning");
+        testInput.add("error");
+        testInput.add("warning");
+        // Lets put some reactive state in
+        BehaviorSubject<HashMap<String, ProcessLogFileResult>> FileToTF_Index$ = BehaviorSubject.createDefault(new HashMap<>());
+        Observable<HashMap<String, Integer>> TermToPresence_Index$ = FileToTF_Index$.map(
+            TFIndex -> {
+                HashMap<String, Integer> documentsWithTerm = new HashMap<>(); // This open the door to modify a file and update TFIndex
+                TFIndex.forEach( // for each file in TFIndex cheap recalculate TPIndex
+                    (file, processLogFileResult) -> { // TFIndex should contain a file entry if file exist even with no terms on it
+                        processLogFileResult.TermsPresence.forEach( // access relevant terms per file
+                            term -> {
+                                Integer nDocuments = documentsWithTerm.get(term); // get the current value for a given term
+                                documentsWithTerm.put(term, nDocuments == null ? 1 : nDocuments + 1); // index the number of files that contains it
+                            }
+                        );
+                    }
+                );
+                testInput.forEach( // initialize terms not found on documents to 0
+                    term -> {
+                        if (!documentsWithTerm.containsKey(term)) {
+                            documentsWithTerm.put(term, 0);
+                        }
+                    }
+                );
+                return documentsWithTerm;
+            }
+        );
+        Observable<HashMap<String, Float>> FileToIDF_Index$ = TermToPresence_Index$.map(
+            TPIndex -> {
+                HashMap<String, Float> IDFIndex = new HashMap<>();
+                HashMap<String, ProcessLogFileResult> TFIndex = FileToTF_Index$.getValue(); // This could also achievable withLatestFrom
+                testInput.forEach(
+                    term -> { // terms that are not in any documents will have a IDFIndex 0
+                        if (TPIndex.get(term) == 0) { // Never divide by 0
+                            IDFIndex.put(term, 0.0f); // since IDF is a log e calc, floor should be 0
+                        } else {
+                            // TFIndex should contain a file entry if file exist even with no terms on it -> TFIndex.size will match number of Files
+                            if (TFIndex.size() == 1) { // IF only 1 document it should be "relevant" instead of having TF*log(1) = 0
+                                IDFIndex.put(term, Float.MIN_VALUE);
+                            } else {
+                                IDFIndex.put(term, (float) log((float) TFIndex.size() / TPIndex.get(term)));
+                            }
+                        }
+                    }
+                );
+                return IDFIndex;
+            }
+        );
+        Observable<HashMap<String, Float>> FileToTF_IDF_Index$ = FileToIDF_Index$.map(
+            IDFIndex -> {
+                HashMap<String, Float> FileToTF_IDF = new HashMap<>();
+                HashMap<String, ProcessLogFileResult> TFIndex = FileToTF_Index$.getValue(); // This could also achievable withLatestFrom
+                // TFIndex should contain a file entry if file exist even with no terms on it
+                TFIndex.forEach(
+                    (file, processLogFileResult) -> {
+                        processLogFileResult.TermsFrequency.forEach(
+                            (term, tf) -> {
+                                FileToTF_IDF.put(file, tf * IDFIndex.get(term));
+                            }
+                        );
+                    }
+                );
+                return FileToTF_IDF;
+            }
+        );
+        FileToTF_IDF_Index$.subscribe(
+            TF_IDF_Index -> {
+                TF_IDF_Index.forEach((file, result) -> System.out.printf("%s -> TF:%s", file, result));
+            },
+            ko -> System.out.printf("\nERROR %s", ko)
+        );
+        File testFile = new File("C:\\Users\\Jesus\\debug.log");
         try {
-            ProcessLogFileResult result = processLogFile(testFile, testInput);
-            System.out.println(result.TermFrequency);
-            result.TermPresence.forEach(s -> System.out.println(s));
+            ProcessLogFileResult result = processLogFile(testFile, testInput); // When the file read operation end
+            HashMap<String, ProcessLogFileResult> TFIndex = FileToTF_Index$.getValue();
+            TFIndex.put(testFile.getCanonicalPath(), result); // No need to test null state
+            FileToTF_Index$.onNext(TFIndex); // Send Event notification for reactions to happen.
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,15 +108,19 @@ public class tfDaemon {
      * Its better not run this function in multiple Threads since HDD performs better when the next read request is to
      * a near sector.
      * And in Parallel scenarios HDD will jump to far sectors while requesting a line from other file.
+     * ---------Edge cases to Test---------
+     * File not exist (removed when the function start) -> This will not happen per enunciate
+     * File is empty
+     * Terms are empty
      * @param file the file to read
-     * @param terms the terms to calculate TermFreq, and TermPresence
-     * @return Object an Anonymous Object with the TF and TP
+     * @param terms the homogenized terms to calculate TermFreq, and TermPresence
+     * @return ProcessLogFileResult static inner class with the TFDictionary and TP
      * @throws IOException exceptions can happen while reading the file
      */
     public static ProcessLogFileResult processLogFile(File file, HashSet<String> terms) throws IOException {
-        HashSet<String> homogenizedTerms = new HashSet<>(); // Case homogenization
-        terms.forEach(s -> homogenizedTerms.add(s.toLowerCase()));
         HashSet<String> totalTermsInDocument = new HashSet<>();
+        HashMap<String, Float> termsFrequency = new HashMap<>();
+        HashSet<String> termsPresence = new HashSet<>();
         HashMap<String, Integer> termAggregation = new HashMap<>();
         // Regex Optimization
         Pattern pattern = Pattern.compile("[a-zA-Z]+");
@@ -54,37 +131,46 @@ public class tfDaemon {
                 matcher.reset(line);
                 while (matcher.find()) {
                     String word = matcher.group().toLowerCase(); // Case homogenization
-                    totalTermsInDocument.add(word);
-                    if (homogenizedTerms.contains(word)) {
+                    totalTermsInDocument.add(word); // Dictionary will remove duplicates
+                    if (terms.contains(word)) {
                         Integer wordAcc = termAggregation.get(word);
                         termAggregation.put(word, wordAcc == null ? 1 : wordAcc + 1);
                     }
                 }
             }
         }
-        float totalTermCount = 0.0f; // It is important to use a float to keep precision in the termFrequency division.
-        for (int termCount : termAggregation.values()) {
-            totalTermCount += termCount;
-        }
-        float termFrequency = 0.0f;
-        if (totalTermsInDocument.size() != 0) { // Never divide by 0
-            termFrequency = totalTermCount / totalTermsInDocument.size();
-        }
-        float finalTermFrequency = termFrequency;
-        HashSet<String> finalTermPresence = new HashSet<>(termAggregation.keySet());
-        return new ProcessLogFileResult(finalTermFrequency, finalTermPresence);
+        terms.forEach( // initialize terms not in file to TF = 0 and populate termsPresence
+            term -> {
+                if (!termAggregation.containsKey(term)) {
+                    termAggregation.put(term, 0);
+                } else {
+                    termsPresence.add(term);
+                }
+            }
+        );
+        // Calculate TF of each term
+        termAggregation.forEach(
+            (term, occurrences) -> {
+                if (totalTermsInDocument.size() == 0) { // if the document has no words (binary?)
+                    termsFrequency.put(term, 0.0f);
+                } else {
+                    termsFrequency.put(term, (float) occurrences / totalTermsInDocument.size()); // will not be divided by 0
+                }
+            }
+        );
+        return new ProcessLogFileResult(termsFrequency, termsPresence);
     }
 
     /**
      * Inner Static class for complex return type.
      */
     public static class ProcessLogFileResult {
-        final Float TermFrequency;
-        final HashSet<String> TermPresence;
+        final HashMap<String, Float> TermsFrequency;
+        final HashSet<String> TermsPresence;
 
-        public ProcessLogFileResult(final Float termFrequency, final HashSet<String> termPresence) {
-            TermFrequency = termFrequency;
-            TermPresence = termPresence;
+        public ProcessLogFileResult(final HashMap<String, Float> termsFrequency, final HashSet<String> termsPresence) {
+            TermsFrequency = termsFrequency;
+            TermsPresence = termsPresence;
         }
     }
 }
